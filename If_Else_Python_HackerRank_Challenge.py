933ac14846181497f0e786688cbf56fe34d8665e
arockiajegan <58635230+arockiajegan@users.noreply.github.com>
2022-05-10 17:05:00 +0530

    Update If_Else_Python_HackerRank_Challenge.py

--- If_Else_Python_HackerRank_Challenge.py
+++ If_Else_Python_HackerRank_Challenge.py
@@ -4,8 +4,8 @@ if n % 2 == 1:
 elif n % 2 == 0 and 2 <= n <= 5:
     print("Not Weird")
 elif n % 2 == 0 and 6 <= n <= 20: 
-    print("Not Weird")
-elif n > 20 : 
+    print("Weird")
+elif n > 20: 
     print ("Not Weird")
 else:
     ("Not Weird")
