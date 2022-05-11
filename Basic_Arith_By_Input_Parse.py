fb591dec29b2e47da4930ec0dd37456c7b57898c
arockiajegan <58635230+arockiajegan@users.noreply.github.com>
2022-05-11 18:42:43 +0530

    Update Basic_Arith_By_Input_Parse.py

--- Basic_Arith_By_Input_Parse.py
+++ Basic_Arith_By_Input_Parse.py
@@ -1,3 +1,7 @@
+#Script Author : AROCKIA JEGAN F
+#visit_me @ www.arcokiajegan.com
+#Description --> Performing basic arithmetic operations between two integers 
+
 a = int(raw_input())
 b = int(raw_input())
 
