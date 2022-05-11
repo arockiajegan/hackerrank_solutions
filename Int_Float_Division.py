3507895f1fa304cb7ad96e7997d28fdbe5fb0822
arockiajegan <58635230+arockiajegan@users.noreply.github.com>
2022-05-11 18:37:32 +0530

    Create Int_Float_Division.py
    Task
    The provided code stub reads two integers, a and b, from STDIN.
    
    Add logic to print two lines. The first line should contain the result of integer division, a // b. The second line should contain the result of float division, a / b.
    
    No rounding or formatting is necessary.

---
+++ Int_Float_Division.py
@@ -0,0 +1,13 @@
+#Script Author : AROCKIA JEGAN F
+#visit_me @ www.arcokiajegan.com
+#Description --> Performing Intiger division and float division between two values 
+from __future__ import division
+
+a = int(raw_input())
+b = int(raw_input())
+
+Intiger_Division = a // b 
+print (Intiger_Division)
+
+Float_Division = a / b
+print (Float_Division)
