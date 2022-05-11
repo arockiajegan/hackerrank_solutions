7c230312998a0bec7ccd527c8c2ac6e0b94340c8
arockiajegan <58635230+arockiajegan@users.noreply.github.com>
2022-05-11 19:06:32 +0530

    Create Square_of_i_lessthan_n.py
    Task
    The provided code stub reads and integer, , from STDIN. For all non-negative integers i<n, print i**2.

---
+++ Square_of_i_lessthan_n.py
@@ -0,0 +1,9 @@
+#Script Author : AROCKIA JEGAN F
+#visit_me @ www.arcokiajegan.com
+#Description --> Sqaring the element i till the limit of n
+
+n = int(raw_input())
+i = 0
+if n > 0 and i < n:
+    for i in range (0, n) : 
+        print (i ** 2)
